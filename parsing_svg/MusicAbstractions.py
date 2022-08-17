d88bbe58300d2fc3d03c796f4fd7b263a531c3e8
Emre Cenk <emrecenk9@gmail.com>
2022-08-17 02:07:55 -0400

    more abstractions and establishing conventions

--- parsing_svg/MusicAbstractions.py
+++ parsing_svg/MusicAbstractions.py
@@ -6,6 +6,14 @@ class Note:
         :param beat_length: How long the note lasts for
         :param note: which note to play
         Todo: figure out how you actually want to store a pitch (an integer should probably do?)
+
+        Note: Mapping notes to beat lengths is shown below:
+        Whole note -> 4
+        Half Note -> 2
+        Quarter Note -> 1
+        Eight Note -> 0.5
+        Sixteenth Note -> 0.25
+        As shown here: https://www.soundbrenner.com/wp-content/uploads/2020/09/note-values.png
         """
         self.beat_length = beat_length
         self.note = note
@@ -17,3 +25,5 @@ class Bar:
 class Piece:
     def __init__(self, bars: List[Bar]):
         self.bars = bars
+        # self.time_signature = ...
+        # todo: finding time signature and simplifying it is not as easy as it seems (i'm not even sure if time signature will be usefull at any point in time anyways)
--- parsing_svg/PathCommands.py
+++ parsing_svg/PathCommands.py
@@ -1,13 +1,17 @@
-#
-#
-# class PathCommandParser:
-#
-#     def __init__(self):
-#         pass
-#
-#     @staticmethod
-#     def path_command_to_music_timings(command: str) -> :
-#         """
-#         :param command: The command to parse
-#         :return:
-#         """
+
+from typing import *
+# from parsing_svg.MusicAbstractions import Note, Bar
+class PathCommandParser:
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def path_command_to_beats(command: str) -> List[int]:
+        """
+        Converts a path command to a list of beats.
+        :param command: The command to parse
+        :return: A list of beats showing how long each note should last
+        Note: look at Note class in parsing_svg/MusicAbstractions.py to see which beat length maps to which integer
+        """
+        pass
